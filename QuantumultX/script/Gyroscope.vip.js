2b10592ae55bcf796e2fbeca1b5f1217beb2dcde
Redeembynight <64399359+Redeembynight@users.noreply.github.com>
2020-11-22 21:14:47 +0800

    记录健康数据全英文app

--- QuantumultX/script/Gyroscope.vip.js
+++ QuantumultX/script/Gyroscope.vip.js
@@ -1,3 +1,10 @@
+/*
+#gyroscope记录健康数据全英文app
+^https:\/\/api\.gyrosco\.pe\/v1\/account\/$ url script-response-body https://raw.githubusercontent.com/langkhach270389/Scripting/master/Gyroscope.vip.js
+
+hostname = api.gyrosco.pe
+*/
+
 let obj = JSON.parse($response.body);
 obj.user["active_until_time"] = "2099-01-01T00:00:00Z";
 $done({body: JSON.stringify(obj)});
