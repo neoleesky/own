dcd19d40bc55530220455fc42f3f276fc9204b90
Redeembynight <64399359+Redeembynight@users.noreply.github.com>
2020-11-25 14:47:43 +0800

    ssh调试工具

---
+++ QuantumultX/script/invalid/Terminus.js
@@ -0,0 +1,12 @@
+/*
+#Termius 
+https:\/\/api\.termius\.com\/api\/v3\/bulk\/account\/ url script-response-body https://raw.githubusercontent.com/langkhach270389/Scripting/master/Terminus.js
+api.termius.com,
+*/
+let obj=JSON.parse($response.body)
+obj.account["pro_mode"] = true;
+obj.account["plan_type"] = "Premium";
+obj.account["user_type"] = "Premium";
+obj.account["current_period"]["until"] = "2099-10-10T03:27:34";
+$done({body:JSON.stringify(obj)})
+//bynubyta
