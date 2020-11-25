bcb5f37fd8f655f8a0e224166b67e0006e92c402
Redeembynight <64399359+Redeembynight@users.noreply.github.com>
2020-11-25 15:29:23 +0800

    Create workingcopy.js

---
+++ QuantumultX/script/invalid/workingcopy.js
@@ -0,0 +1,7 @@
+/*
+#workingcopy
+^https:\/\/education\.github\.com\/api\/user$ url script-response-body https://raw.githubusercontent.com/langkhach270389/Scripting/master/workingcopy.js
+
+education.github.com
+*/
+$done({body: '{"student": true}'})
