1a2e05c28af116802e46679564daabafb1f76978
Redeembynight <64399359+Redeembynight@users.noreply.github.com>
2020-12-09 15:12:29 +0800

    mojo&noto

---
+++ QuantumultX/script/revenuecat.js
@@ -0,0 +1,26 @@
+/*
+#mojo&noto
+#^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/[a-zA-Z0-9_-]*$) url script-response-body https://raw.githubusercontent.com/langkhach270389/Scripting/master/revenuecat.js
+api.revenuecat.com, 
+*/
+let obj = JSON.parse($response.body);
+obj["subscriber"]["subscriptions"]= {
+      "revenuecat.pro.yearly": {
+        "is_sandbox": false,
+        "period_type": "active",
+        "billing_issues_detected_at": null,
+        "unsubscribe_detected_at": null,
+        "expires_date": "2099-12-01T03:51:32Z",
+        "original_purchase_date": "2019-10-31T02:51:33Z",
+        "purchase_date": "2019-10-31T02:51:32Z",
+        "store": "app_store"
+      }
+    };
+obj["subscriber"]["entitlements"]= {
+      "pro": {
+        "expires_date": "2099-12-01T03:51:32Z",
+        "product_identifier": "revenuecat.pro.yearly",
+        "purchase_date": "2019-10-31T02:51:32Z"
+      }
+    };
+$done({body: JSON.stringify(obj)});
