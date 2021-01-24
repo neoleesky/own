61b584a58f7c544d51e5e9a3bd615649022c083e
Redeembynight <64399359+Redeembynight@users.noreply.github.com>
2021-01-24 10:31:45 +0800

    health ios only

--- QuantumultX/script/Gyroscope.vip.js
+++ QuantumultX/script/Gyroscope.vip.js
@@ -2,6 +2,7 @@
 #gyroscope记录健康数据全英文app
 ^https:\/\/api\.gyrosco\.pe\/v1\/account\/$ url script-response-body https://raw.githubusercontent.com/langkhach270389/Scripting/master/Gyroscope.vip.js
 hostname = api.gyrosco.pe
+
 */
 
 let obj = JSON.parse($response.body);
