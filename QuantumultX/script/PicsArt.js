9a05ece7b161ffb009dda74d8e1f0ef630555fa7
Redeembynight <64399359+Redeembynight@users.noreply.github.com>
2021-01-30 17:12:57 +0800

    sticker,meme&collage maker

--- QuantumultX/script/PicsArt.js
+++ QuantumultX/script/PicsArt.js
@@ -1,6 +1,5 @@
 /*
 PicsArt app unlocks pro
-
 Surge4.0:
 http-response https:\/\/api\.(picsart|meiease)\.c(n|om)\/users\/show\/me\.json requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/PicsArt.js
 
