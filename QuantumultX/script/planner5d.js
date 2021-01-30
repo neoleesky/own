7d3d59a99a91a215695718ef11a70bfad7cd4faa
Redeembynight <64399359+Redeembynight@users.noreply.github.com>
2021-01-30 17:07:46 +0800

    plan & design your dream home

---
+++ QuantumultX/script/planner5d.js
@@ -0,0 +1,24 @@
+/*
+#planner5d
+^https:\/\/planner5d\.com\/api\/sets url script-response-body https://raw.githubusercontent.com/langkhach270389/Scripting/master/planner5d.js
+
+planner5d.com,
+*/
+var obj = JSON.parse($response.body);
+
+obj=
+{
+  "accessDays": 999999999,
+  "allSets": 1,
+  "error": 0,
+  "m": {
+    "r": "999999999",
+    "useSubscriptions": true,
+    "expiredSubscription": "999999999",
+    "subscription": "com.planner5d.planner5d.subscription.yearly"
+  }
+};
+
+$done({body: JSON.stringify(obj)});
+
+// Descriptions
