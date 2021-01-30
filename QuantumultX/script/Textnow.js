af160d8778211c88148e7fbe3dcb27d3af6989c8
Redeembynight <64399359+Redeembynight@users.noreply.github.com>
2021-01-30 17:10:09 +0800

    anti-ads

--- QuantumultX/script/Textnow.js
+++ QuantumultX/script/Textnow.js
@@ -1,7 +1,8 @@
 /*
 #Textnow
 ^https:\/\/api\.textnow\.me\/api2.0\/users\/.* url script-response-body https://raw.githubusercontent.com/langkhach270389/Scripting/master/Textnow.js
- api.textnow.me,
+
+api.textnow.me,
 */
 var obj = JSON.parse($response.body); 
 obj['show_ads'] = false;
