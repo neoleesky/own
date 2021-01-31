3ab7602fef5496deac8d235131bef96deae2945d
Redeembynight <64399359+Redeembynight@users.noreply.github.com>
2021-01-31 17:13:35 +0800

    Update xcx.js

--- QuantumultX/script/mini program in wechat/to test/xcx.js
+++ QuantumultX/script/mini program in wechat/to test/xcx.js
@@ -2,8 +2,10 @@
 Unlocks by Cuttlefish 公众号：墨鱼手记
 # > 043 森茂雅苑 微信小程序解锁隐藏内容 去除广告直接查看内容（20201027）
 https://d.smkj33.top/wp-json/wp/v2/posts/* url script-response-body https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Crack/xcx.js
-
-d.smkj33.top,
+mitm=d.smkj33.top,
+# > 030 老胡工具箱 微信小程序解除隐藏内容 （20200916）
+https://d.syshhc.top/wp-json/wp/v2/* url script-response-body https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Crack/xcx.js
+d.syshhc.top, 
 */
 
 re('"excitationAd":"\\d"@Ad":"\d"@ad":true@AdId":"[^"]*"@adid":"[^"]*"','"excitationAd":"0"@Ad":"0"@ad":false@AdId":""@adid":""')
