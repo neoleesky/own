ad925cf6b7c4b98ef3ea950ffcc538b75443a8ca
Redeembynight <64399359+Redeembynight@users.noreply.github.com>
2021-01-31 17:00:26 +0800

    森茂雅苑

---
+++ QuantumultX/script/mini program in wechat/to test/xcx.js
@@ -0,0 +1,26 @@
+/*
+Unlocks by Cuttlefish 公众号：墨鱼手记
+# > 043 森茂雅苑 微信小程序解锁隐藏内容 去除广告直接查看内容（20201027）
+https://d.smkj33.top/wp-json/wp/v2/posts/* url script-response-body https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Crack/xcx.js
+
+d.smkj33.top,
+*/
+
+re('"excitationAd":"\\d"@Ad":"\d"@ad":true@AdId":"[^"]*"@adid":"[^"]*"','"excitationAd":"0"@Ad":"0"@ad":false@AdId":""@adid":""')
+
+function re() {
+ var body = $response.body;
+ if (arguments[0].includes("@")) {
+  var regs = arguments[0].split("@");
+  var strs = arguments[1].split("@");
+  for (i = 0;i < regs.length;i++) {
+   var reg = new RegExp(regs[i],"g");
+   body = body.replace(reg, strs[i]);
+ }
+}
+ else {
+  var reg = new RegExp(arguments[0],"g");
+  body = body.replace(reg, arguments[1]);
+}
+ $done(body);
+} 
