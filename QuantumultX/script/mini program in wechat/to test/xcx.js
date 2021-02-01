f021d80a629ae863df0a683a5f667cdb4700865c
Redeembynight <64399359+Redeembynight@users.noreply.github.com>
2021-02-01 12:48:16 +0800

    Update xcx.js

--- QuantumultX/script/mini program in wechat/to test/xcx.js
+++ QuantumultX/script/mini program in wechat/to test/xcx.js
@@ -9,6 +9,21 @@ api.bspapp.com,
 # > 030 老胡工具箱 微信小程序解除隐藏内容 （20200916）
 https://d.syshhc.top/wp-json/wp/v2/* url script-response-body https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Crack/xcx.js
 d.syshhc.top, 
+# > 006 酸鸡汤,老爷花苑（光头的小程序，两个通用）（20200916）
+https://[a-z]+.ddly666.top/wp-json/wp/v2/posts/* url script-response-body https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Crack/xcx.js
+ *.ddly666.top, 
+# > 005 黑科技软件（20200901）小程序解除隐藏内容
+^https:\/\/hkj178.com/wp-json/mp/v2/posts/* url script-response-body https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Crack/xcx.js
+hkj178.com, 
+# > 004 天泽后花园（20200901）小程序解除隐藏内容
+^https:\/\/a.jxjt888.top/wp-json/wp/v2/posts/* url script-response-body https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Crack/xcx.js
+a.jxjt888.top, 
+# > 003 小野分享（20200901）小程序解除隐藏内容
+^https:\/\/lysl2020.com/wp-json/wp/v2/posts/* url script-response-body https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Crack/xcx.js
+lysl2020.com, 
+# > 002 搜罗好货（20200901）小程序解除隐藏内容
+^https:\/\/www.i3zh.com/wp-json/wp/v2/posts/* url script-response-body https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Crack/xcx.js
+www.i3zh.com, 
 */
 
 re('"excitationAd":"\\d"@Ad":"\d"@ad":true@AdId":"[^"]*"@adid":"[^"]*"','"excitationAd":"0"@Ad":"0"@ad":false@AdId":""@adid":""')
