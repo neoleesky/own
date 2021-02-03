7db02d89abae84b8cc7ad75f8ca11d9f04e5b06c
Redeembynight <64399359+Redeembynight@users.noreply.github.com>
2021-02-03 14:17:17 +0800

    to do list

--- QuantumultX/script/unlock/WanDouQingDan.js
+++ QuantumultX/script/unlock/WanDouQingDan.js
@@ -1,6 +1,9 @@
 /*
 Unlocks by Cuttlefish 公众号：墨鱼手记
-# > 050 豌豆清单 为你的日程好好规划一下(20201112)
+#豌豆清单 为你的日程好好规划一下(20201112)
+
+下载地址 https://apps.apple.com/us/app/%E8%B1%8C%E8%B1%86%E6%B8%85%E5%8D%95-%E6%8F%90%E9%86%92%E4%BA%8B%E9%A1%B9%E6%97%A5%E5%8E%86%E6%89%93%E5%8D%A1to-do-list/id1477839383
+
 ^https:\/\/www\.40sishi\.com\/list\/user\/profile$ url script-response-body https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Crack/wdqd.js
 
 www.40sishi.com, 
