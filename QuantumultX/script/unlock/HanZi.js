da9548cca7728bd89cb1cec5cb17ae8753f872ba
Redeembynight <64399359+Redeembynight@users.noreply.github.com>
2021-02-03 12:15:48 +0800

    origin of chinese characters

--- QuantumultX/script/unlock/HanZi.js
+++ QuantumultX/script/unlock/HanZi.js
@@ -1,6 +1,9 @@
 /*
 Unlocks by Cuttlefish 公众号：墨鱼手记
-# > 037 汉字的故事 解锁会员字体 探寻古汉字之美（20201002）
+#汉字的故事 解锁会员字体 探寻古汉字之美（20201002）
+
+下载地址 https://apps.apple.com/us/app/%E6%B1%89%E5%AD%97%E7%9A%84%E6%95%85%E4%BA%8B-%E6%8E%A2%E7%B4%A2%E6%B1%89%E5%AD%97%E8%B5%B7%E6%BA%90/id1439752412
+
 https://avoscloud.com/1.1/* url script-response-body https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Crack/hzdgs.js
 
 avoscloud.com, 
