e7042627c461ffd71b9e835b4025c3e94dcd139e
Redeembynight <64399359+Redeembynight@users.noreply.github.com>
2021-02-03 14:30:40 +0800

    books

--- QuantumultX/script/unlock/NeteaseSnailReading.js
+++ QuantumultX/script/unlock/NeteaseSnailReading.js
@@ -1,9 +1,12 @@
 /*
-
 Netease snail reading unlock vip
 By yxiaocai & JO2EY
+
+下载地址 https://apps.apple.com/us/app/%E7%BD%91%E6%98%93%E8%9C%97%E7%89%9B%E8%AF%BB%E4%B9%A6/id1127249355
+
 QX:
 ^https?:\/\/p\.du\.163\.com\/gain\/readtime\/info\.json url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/wnyd.js
+
 Surge4：
 http-response ^https?:\/\/p\.du\.163\.com\/gain\/readtime\/info\.json requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/wnyd.js
 
