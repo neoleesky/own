1e98ed926d7cf368eca5de0f21014edf3539adcb
Redeembynight <64399359+Redeembynight@users.noreply.github.com>
2021-02-03 12:44:11 +0800

    insta story editor

--- QuantumultX/script/unlock/mojo.js
+++ QuantumultX/script/unlock/mojo.js
@@ -1,6 +1,10 @@
 /*
 #mojo
+
+下载地址 https://apps.apple.com/us/app/mojo-insta-story-editor/id1434861974
+
 #^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/[a-zA-Z0-9_-]*$) url script-response-body https://raw.githubusercontent.com/Redeembynight/own/main/QuantumultX/script/mojo.js
+
 api.revenuecat.com, 
 */
 let obj["subscriber"]["entitlements"]= {
