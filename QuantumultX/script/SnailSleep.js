6151e829491ad988e84fc4b4a47fefb1e2791b02
Redeembynight <64399359+Redeembynight@users.noreply.github.com>
2021-02-03 14:20:47 +0800

    dreamtalk recorder

--- QuantumultX/script/SnailSleep.js
+++ QuantumultX/script/SnailSleep.js
@@ -1,6 +1,10 @@
 /*
 #蜗牛睡眠 全功能解锁VIP&付费音乐
-^https:\/\/(snailsleep\.net\/|(music|community)\.snailsleep\.net\/)(snail\/v1\/profile\/get|snail-music\/music\/(sleeping|meditation)\/single\/list) url script-response-body wnsm1.js
+
+下载地址 https://apps.apple.com/us/app/snail-sleep-dreamtalk-recorder/id1025313530
+
+^https:\/\/(snailsleep\.net\/|(music|community)\.snailsleep\.net\/)(snail\/v1\/profile\/get|snail-music\/music\/(sleeping|meditation)\/single\/list) url script-response-body https://raw.githubusercontent.com/Redeembynight/own/main/QuantumultX/script/wnsm.js
+
 MITM = snailsleep.net,music.snailsleep.net,community.snailsleep.net
 */
 
