ac4d44e54d15c7931344986be4fe062be2645c4d
Redeembynight <64399359+Redeembynight@users.noreply.github.com>
2021-02-03 13:45:00 +0800

    graphic design maker

--- QuantumultX/script/unlock/over.vip.js
+++ QuantumultX/script/unlock/over.vip.js
@@ -1,6 +1,11 @@
 /*
 #over
+越南老哥langkhach270389
+
+下载地址 https://apps.apple.com/us/app/over-graphic-design-maker/id535811906
+
 ^https:\/\/api\.overhq\.com\/(user\/token\/refresh$|subscription\/verifyReceipt$) url script-response-body https://raw.githubusercontent.com/langkhach270389/Scripting/master/over.vip.js
+
 api.overhq.com, 
 */
 var body = $response.body;
