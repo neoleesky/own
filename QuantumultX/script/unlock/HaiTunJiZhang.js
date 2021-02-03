96d4b0126e936e071e70fe5ac381d87d958f6771
Redeembynight <64399359+Redeembynight@users.noreply.github.com>
2021-02-03 11:31:17 +0800

    organize money

--- QuantumultX/script/unlock/HaiTunJiZhang.js
+++ QuantumultX/script/unlock/HaiTunJiZhang.js
@@ -1,5 +1,7 @@
 /*
 #海豚记账 (by @superuv)
+下载地址 https://apps.apple.com/us/app/%E8%AE%B0%E8%B4%A6-%E6%B5%B7%E8%B1%9A%E8%AE%B0%E8%B4%A6%E6%9C%AC-%E6%97%A5%E5%B8%B8%E8%A3%85%E4%BF%AE%E6%97%85%E8%A1%8C%E8%AE%B0%E8%B4%A6%E5%B7%A5%E5%85%B7/id1232616736
+
 https:\/\/book\.haitunwallet\.com\/app\/vip\/status url script-response-body https://raw.githubusercontent.com/nzw9314/QuantumultX/master/Script/HTJZ.js
 
 book.haitunwallet.com, 
