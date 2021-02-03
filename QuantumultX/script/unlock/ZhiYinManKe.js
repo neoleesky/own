95e48125411003fea3cd25792630fa47ada77853
Redeembynight <64399359+Redeembynight@users.noreply.github.com>
2021-02-03 11:42:51 +0800

    comics

--- QuantumultX/script/unlock/ZhiYinManKe.js
+++ QuantumultX/script/unlock/ZhiYinManKe.js
@@ -1,6 +1,9 @@
 /*
 知音漫客 unlock vip
 by mieqq
+
+下载地址 https://apps.apple.com/us/app/%E7%9F%A5%E9%9F%B3%E6%BC%AB%E5%AE%A2-%E7%9C%8B%E4%BA%8C%E6%AC%A1%E5%85%83%E6%BC%AB%E7%94%BB%E5%B9%B3%E5%8F%B0%E5%A4%A7%E5%85%A8%E5%BF%AB/id1012491820
+
 QX:
 ^https://getuserinfo-globalapi.zymk.cn/app_api/v5/(getuserinfo|coin_account|getuserinfo_ticket|getcomicinfo)/ url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/Zymh.js
 
