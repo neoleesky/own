066fe8ed36eb213e8b7d5a1da1bf89e824d86f36
Redeembynight <64399359+Redeembynight@users.noreply.github.com>
2021-02-03 12:17:47 +0800

    funny ecards, videos & gifs

--- QuantumultX/script/unlock/jibjab.vip.js
+++ QuantumultX/script/unlock/jibjab.vip.js
@@ -1,5 +1,9 @@
 /*
 # Jibjab(funny ecards,videos&gifs)
+越南老哥langkhach270389
+
+下载地址 https://apps.apple.com/us/app/jibjab/id875561136
+
 ^https:\/\/origin-prod-phoenix\.jibjab\.com\/v1\/user$ url script-response-body https://raw.githubusercontent.com/langkhach270389/Scripting/master/jibjab.vip.js
 
  api.sync.me,
