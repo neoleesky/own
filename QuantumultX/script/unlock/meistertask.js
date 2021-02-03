7e9f22c955c96d2abbecb62e8717d80e5ce6e250
Redeembynight <64399359+Redeembynight@users.noreply.github.com>
2021-02-03 12:32:05 +0800

    work, kanban & project planner

--- QuantumultX/script/unlock/meistertask.js
+++ QuantumultX/script/unlock/meistertask.js
@@ -1,6 +1,9 @@
 /*
 Unlocks by Cuttlefish 公众号：墨鱼手记
-# > 024 MeisterTask Premium(20200910)
+#MeisterTask Premium(20200910)
+
+下载地址 https://apps.apple.com/us/app/task-management-meistertask/id918099883
+
 https://www.mindmeister.com/api/v2/licenses/meistertask url script-response-body https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Crack/meistertask.js
 
 www.mindmeister.com, 
