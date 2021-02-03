7622770080d35770a6b9e90224a3cc3eeacc6f56
Redeembynight <64399359+Redeembynight@users.noreply.github.com>
2021-02-03 12:45:49 +0800

    language

--- QuantumultX/script/unlock/mondly.vip.js
+++ QuantumultX/script/unlock/mondly.vip.js
@@ -1,6 +1,11 @@
 /*
 #mondly
+越南老哥langkhach270389
+
+下载地址 https://apps.apple.com/us/app/learn-33-languages-with-mondly/id987873536
+
 ^https:\/\/api\.mondlylanguages\.com\/v1\/ios\/user\/sync$ url script-response-body https://raw.githubusercontent.com/langkhach270389/Scripting/master/mondly.vip.js
+
 mondly.vip.js
 */
 var obj = JSON.parse($response.body);
