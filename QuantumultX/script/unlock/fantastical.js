23723b6596cd9b31779ef8cc70bc2e0be530afa6
Redeembynight <64399359+Redeembynight@users.noreply.github.com>
2021-02-03 12:11:07 +0800

    calendar & tasks

--- QuantumultX/script/unlock/fantastical.js
+++ QuantumultX/script/unlock/fantastical.js
@@ -1,6 +1,9 @@
 /*
 Unlocks by Cuttlefish 公众号：墨鱼手记
-# > 032 Fantastical 解锁SVIP会员订阅 （20200924）⭐️
+#Fantastical 解锁SVIP会员订阅 （20200924）⭐️
+
+下载地址 https://apps.apple.com/us/app/fantastical-calendar-tasks/id718043190
+
 ^https:\/\/api\.flexibits\.com\/(v1\/auth\/device|v1\/account\/details) url script-response-body https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Crack/fantastical.js
 
 api.flexibits.com, 
