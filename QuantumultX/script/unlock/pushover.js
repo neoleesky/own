c584d620688008d0b403cf7c0247f502b9317d18
Redeembynight <64399359+Redeembynight@users.noreply.github.com>
2021-02-03 13:55:11 +0800

    push notification

--- QuantumultX/script/unlock/pushover.js
+++ QuantumultX/script/unlock/pushover.js
@@ -1,6 +1,11 @@
 /*
 #pushover
+越南老哥langkhach270389
+
+下载地址 https://apps.apple.com/us/app/pushover-notifications/id506088175
+
 ^https:\/\/api\.pushover\.net\/1\/messages\.json* url script-response-body https://raw.githubusercontent.com/langkhach270389/Scripting/master/pushover.js
+
 api.pushover.net, 
 */
 var obj = JSON.parse($response.body);
