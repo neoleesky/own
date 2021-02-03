8c288b6bce9afc1e1cb3c1f76205126eeba8e504
Redeembynight <64399359+Redeembynight@users.noreply.github.com>
2021-02-03 12:54:34 +0800

    vietnam music app

--- QuantumultX/script/unlock/nhaccuatui.js
+++ QuantumultX/script/unlock/nhaccuatui.js
@@ -1,5 +1,10 @@
 /*
 #Nhaccuatui
+越南老哥langkhach270389
+
+下载地址
+https://apps.apple.com/us/app/nhaccuatui-find-mp3-music/id1210763463
+
 ^https:\/\/graph\.nhaccuatui\.com\/.*\/users\/info* url script-response-body https://raw.githubusercontent.com/langkhach270389/Scripting/master/nhaccuatui.js
 
 graph.nhaccuatui.com,
