d9c0e9907debde3c720b3fb3906bfe9cd3ffe0d3
Redeembynight <64399359+Redeembynight@users.noreply.github.com>
2021-02-03 16:30:16 +0800

    Update xcx.js

--- QuantumultX/script/mini program in wechat/to test/xcx.js
+++ QuantumultX/script/mini program in wechat/to test/xcx.js
@@ -1,14 +1,61 @@
 /*
 Unlocks by Cuttlefish 公众号：墨鱼手记
-# > 043 森茂雅苑 微信小程序解锁隐藏内容 去除广告直接查看内容（20201027）
-https://d.smkj33.top/wp-json/wp/v2/posts/* url script-response-body https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Crack/xcx.js
-mitm=d.smkj33.top,
+# > 026 科技窝（20201228）
+https://www.benbenfx.xyz/wp-json/wp/v2/posts url script-response-body https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Crack/xcx.js
+www.benbenfx.xyz
+# > 024 电视盒子君（20201218）
+https://tvapp.guilaile.cn/wp-json/wp/v2/posts url script-response-body https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Crack/xcx.js
+tvapp.guilaile.cn
+# > 023 公子黑科技（20201218）
+https://gz.gongzijx.com/wp-json/wp/v2/posts url script-response-body https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Crack/xcx.js
+gz.gongzijx.com
+# > 022 世颜说 （20201217）
+https://d.syshhc.top/wp-json/wp/v2/posts url script-response-body https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Crack/xcx.js
+d.syshhc.top
+# > 021 搞机师兄（20201216）
+https://gjsx.vip/wp-json/wp/v2/posts url script-response-body https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Crack/xcx.js
+gjsx.vip
+# > 020 火箭软件库（20201216）
+https://jt.jxjt888.top/wp-json/wp/v2/posts url script-response-body https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Crack/xcx.js
+jt.jxjt888.top
+# > 019 天书乱谈（20201215）
+https://[a-z]+.tslt.xyz/wp-json/wp/v2/posts url script-response-body https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Crack/xcx.js
+*.tslt.xyz
+# > 017 天泽百宝箱（20201215）
+https://c.jxjt888.top/wp-json/wp/v2/posts url script-response-body https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Crack/xcx.js
+c.jxjt888.top/wp-json
+# > 016 老鬼黑科技（20201215）
+https://www.laoguikeji.cn/wp-json/wp/v2/posts url script-response-body https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Crack/xcx.js
+www.laoguikeji.cn
+# > 015 黑科技软件源（20201215）
+https://qj.bpojie.com/wp-json/wp/v2/posts url script-response-body https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Crack/xcx.js
+qj.bpojie.com
+# > 014 天书X乱谈（20201215）
+https://b.tslt.xyz/wp-json/wp/v2/posts url script-response-body https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Crack/xcx.js
+b.tslt.xyz/wp-json
+# > 013 图层黑科技（20201215）
+https://m.xgjyouhui.com/wp-json/wp/v2/posts url script-response-body https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Crack/xcx.js
+m.xgjyouhui.com
+# > 012 free 黑科技（20201215）
+https://www.freeheikeji.cn/wp-json/wp/v2/posts url script-response-body https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Crack/xcx.js
+www.freeheikeji.cn
+# > 011 小纸箱助手（20201215）
+https://xcx.xianbaow.com/wp-json/wp/v2/posts url script-response-body https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Crack/xcx.js
+xcx.xianbaow.com
+# > 009 AppleAndroid 弹框显示内容（20201214）
+https://iosoi.cn/wp-json/wp/v2/posts/\d+ url script-response-body https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Crack/xcx.js
+# > 辅助去除页面嵌入式式卡片广告（20201214）
+https://iosoi.cn/wp-content/plugins url script-response-body https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Crack/xcx.js
+iosoi.cn
+# > 025 森茂小苑 （20201219）
+https://[a-z]+.smkj33.top/wp-json/wp/v2/posts url script-response-body https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Crack/xcx.js
+*.smkj33.top
 # > 038 纸塘壁纸 微信小程序 免广告直接下载原图壁纸（20201006）Attention：登录使用⭐️
 https://api.bspapp.com/client url script-response-body https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Crack/ztbz.js
 api.bspapp.com, 
-# > 030 老胡工具箱 微信小程序解除隐藏内容 （20200916）
-https://d.syshhc.top/wp-json/wp/v2/* url script-response-body https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Crack/xcx.js
-d.syshhc.top, 
+# > 018 老胡群英会（20201215）
+https://g.syshhc.top/wp-json/wp/v2/posts url script-response-body https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Crack/xcx.js
+g.syshhc.top
 # > 006 酸鸡汤,老爷花苑（光头的小程序，两个通用）（20200916）
 https://[a-z]+.ddly666.top/wp-json/wp/v2/posts/* url script-response-body https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Crack/xcx.js
  *.ddly666.top, 
